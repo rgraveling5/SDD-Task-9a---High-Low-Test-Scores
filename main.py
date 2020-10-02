17d73939e77b8c833b18b30abf3ddcad41a3eab0
Rachael Graveling <clrg092@cl.glow.scot>
2020-10-02 19:02:54 +0000

    Finished and formatted

--- main.py
+++ main.py
@@ -10,21 +10,24 @@ names = []
 scores = []
 
 for x in range(3):
-  names.append(input("Name: "))
-  scores.append(input("Scores: "))
+    name = input('Name: ')
+    score = int(input('Score: '))
+
+    names.append(name)
+    scores.append(score)
+print()
 
 maximum = scores[0]
 for x in range(len(scores)):
-    if maximum > scores[x]:
+    if scores[x] > maximum:
         maximum = scores[x]
-        
-print("The highest score is " + str(maximum))
-print()
+        max_name = names[x]
+print('The highest score is ' + str(maximum) + ' scored by ' + max_name)
 
 minimum = scores[0]
-for x in range(len(scores)):
-    if minimum < scores[x]:
-        minimum = scores[x]
-print("The highest score is " + str(minimum) + "by" )
+for i in range(len(scores)):
+    if scores[i] <= minimum:
+        minimum = scores[i]
+        min_name = names[i]
+print('The lowest score is ' + str(minimum) + ' scored by ' + min_name)
 print()
-
